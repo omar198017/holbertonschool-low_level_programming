9e3d934e5741c7cf21e6b8c7d0d4462a5e280f80
omar198017 <omar198017@yahoo.com>
2025-10-19 15:04:39 -0500

    Write a 64-bit program in assembly that prints Hello, World, followed by a new line.

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -3,30 +3,17 @@
 ; Author: Omar Caguazango
 ; Assembled with: nasm -f elf64 100-hello_world.asm && gcc -no-pie -std=gnu89 100-hello_world.o -o hello
 
-section .data
-    msg db "Hello, World", 10  ; 10 is ASCII for newline '\n'
-    len equ $ - msg            ; Calculate length of string
+	global	main
 
-section .text
-    global _start
+	section	.text
+main:	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, message
+	mov	rdx, 17
+	syscall
+	mov	rax, 60
+	xor	rdi, rdi
+	syscall
 
-_start:
-    ; syscall: write(int fd, const void *buf, size_t count)
-    ; syscall number for write: 1
-    ; fd = 1 (stdout)
-    ; buf = pointer to msg
-    ; count = len
-
-    mov     rax, 1      ; syscall number for write
-    mov     rdi, 1      ; file descriptor: stdout
-    mov     rsi, msg    ; buffer to write
-    mov     rdx, len    ; length of message
-    syscall             ; invoke system call
-
-    ; syscall: exit(int status)
-    ; syscall number: 60
-    ; status: 0
-
-    mov     rax, 60     ; syscall number for exit
-    xor     rdi, rdi    ; status 0
-    syscall             ; invoke system call
+	section	.data
+message:db	"Hello, Holberton", 10
